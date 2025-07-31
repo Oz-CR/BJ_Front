1463fdb65eb1d20f7fc1e9231e508b55622f8224
MisaelTP <trejoperez.misael2345@gmail.com>
2025-07-31 00:03:07 -0600

    modal

--- src/app/pages/rooms/rooms.ts
+++ src/app/pages/rooms/rooms.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CradGame } from '../../components/crad-game/crad-game';
+import { FormRoom } from '../../components/form-room/form-room';
 
 @Component({
   selector: 'app-rooms',
-  imports: [CradGame],
+  imports: [CradGame, FormRoom],
   templateUrl: './rooms.html',
   styleUrl: './rooms.css',
 })
